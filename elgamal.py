19139feda9072b9de56d44a21158b7c19bec0e01
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-01-25 18:29:57 -0500

    changed based on ED

--- elgamal.py
+++ elgamal.py
@@ -4,16 +4,14 @@ from params import p
 from params import g
 
 def keygen():
-    a = random.randint(1, p)
-    sk = a #int in range 1..p
-    pk = pow(g, a, mod=p) #g^a mod p
+    sk = random.randint(1, p)
+    pk = pow(g, sk, p) #g^a mod p
     return pk,sk
 
 def encrypt(pk,m):
-    r = random.randint(1,p) #q?
-    c1 = pow(g, r, mod=p)
-    c2 = pow(pk,r, mod=p)
-    #what do i do with m here
+    r = random.randint(1,p)
+    c1 = pow(g, r, p)
+    c2 = pk**r*m % p
     return [c1,c2]
 
 def decrypt(sk,c):
@@ -24,7 +22,7 @@ def decrypt(sk,c):
 
 def main():
     keygen()
-    decrypt(0,[1,2])
+    #decrypt(0,[1,2])
     #encrypt(0,0)
     print("hello")
 
