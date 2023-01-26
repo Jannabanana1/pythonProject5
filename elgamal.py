3e03fb0a67fd932ad5c1c69ac6c8c3336451edab
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-01-26 15:07:03 -0500

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -11,20 +11,16 @@ def keygen():
 def encrypt(pk,m):
     r = random.randint(1,p)
     c1 = pow(g, r, p)
-
     c2 = pow(pk,r,p)
     c2 = c2* m %p
     return [c1,c2]
 
 
 def mod_inverse(a, m):
-    pass
+    pow(a,-1,m)
 
 
 def decrypt(sk,c):
-    #c2 = c[1]
-    #c1 = c[0]
     m = mod_inverse(pow(c[0],sk,p),p)
     m = m*c[1] %p
-    #(c2/(c1**sk)) % sk
     return m
