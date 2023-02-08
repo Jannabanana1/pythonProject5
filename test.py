ffbb8a028050e55b21dc922e76784ea3b7070180
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-02-08 16:20:08 -0500

    testing

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 #just a test again and again
+
+def fib(self, n: int) -> int:
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return self.fib(n-2) + self.fib(n-1)
+
+def main():
+    fib(self, 3)
+
+if __name__ == "__main__":
+    main()
