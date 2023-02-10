4496671b3938ba0287112528eef59df883edba8c
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-02-09 22:32:39 -0500

    testing again

--- ipfs.py
+++ ipfs.py
@@ -7,7 +7,7 @@ def pin_to_ipfs(data):
     #YOUR CODE HERE
     data_str = str(data)
     response = requests.post('https://ipfs.infura.io:5001/api/v0/add',
-                             files={'file' : data_str},auth=('2L58AfT67nPxin5ayTe2iHI4Qkg','02e45d5f2415b3b035aa1eb04bdfa1f2'))
+                             files={'file' : data_str},auth=('2LVaqfpTeMvb7NuwrPSBBNvSL2u','17e2b174612e5eea2af96e5b7f55ebbf'))
     #print(response)
     return_value = response.json()
     cid = return_value['Hash']
@@ -19,7 +19,7 @@ def get_from_ipfs(cid,content_type="json"):
     params = (
     ('arg',cid),
     )
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2L58AfT67nPxin5ayTe2iHI4Qkg','02e45d5f2415b3b035aa1eb04bdfa1f2'))
+    response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2LVaqfpTeMvb7NuwrPSBBNvSL2u','17e2b174612e5eea2af96e5b7f55ebbf'))
     #print(response)
     #print(response.text)
     data = eval(response.text)
