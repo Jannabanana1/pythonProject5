ef2227b91442d140695c0edb90bf2544b847f30c
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-02-09 22:30:40 -0500

    testing again

--- ipfs.py
+++ ipfs.py
@@ -3,32 +3,33 @@ import json
 
 
 def pin_to_ipfs(data):
-    assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
-    # create a variable to store file as data, convert to string adn send it to infura mentionaed in guide
-    # pass credentials and endpt to infura website
-    # str = json.dumps(data)
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=data, auth=("2LVaqfpTeMvb7NuwrPSBBNvSL2u"
-                                                                                         ,
-                                                                                         "17e2b174612e5eea2af96e5b7f55ebbf"))
-    print(response.text)
-    cid = response.json()["Hash"]
-    print(cid)
+    assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+    #YOUR CODE HERE
+    data_str = str(data)
+    response = requests.post('https://ipfs.infura.io:5001/api/v0/add',
+                             files={'file' : data_str},auth=('2L58AfT67nPxin5ayTe2iHI4Qkg','02e45d5f2415b3b035aa1eb04bdfa1f2'))
+    #print(response)
+    return_value = response.json()
+    cid = return_value['Hash']
     return cid
-
-
-def get_from_ipfs(cid, content_type="json"):
-    # get cid and create that and pass object parameter and need to make api call with credentials and cid
-    # change str to json object
-    # change json to dictionary and then return
-    assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
-    # YOUR CODE HERE
-
-    #assert isinstance(data, dict), f"get_from_ipfs should return a dict"
-
+def get_from_ipfs(cid,content_type="json"):
+    assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+    #YOUR CODE HERE
+    url = 'https://ipfs.infura.io:5001/api/v0/cat?arg=' + cid
     params = (
-        ('arg', cid),
+    ('arg',cid),
     )
-    data = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params,
-                     auth=("2LVaqfpTeMvb7NuwrPSBBNvSL2u", "17e2b174612e5eea2af96e5b7f55ebbf"))
-    print(data)
+    response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=('2L58AfT67nPxin5ayTe2iHI4Qkg','02e45d5f2415b3b035aa1eb04bdfa1f2'))
+    #print(response)
+    #print(response.text)
+    data = eval(response.text)
+    assert isinstance(data,dict), f"get_from_ipfs should return a dict"
     return data
+
+
+
+
+
+
+
+
