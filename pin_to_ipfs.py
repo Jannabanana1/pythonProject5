47aed37d5c3d0b509c6bbe0a7a3f3745b2456804
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-02-09 19:35:13 -0500

    testing again

--- pin_to_ipfs.py
+++ pin_to_ipfs.py
@@ -2,18 +2,20 @@ import requests
 import json
 
 
-def get_from_ipfs(cid,content_type="json"):
-    #get cid and create that and pass object parameter and need to make api call with credentials and cid
-    #change str to json object
-    #change json to dictionary and then return
-	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
-	#YOUR CODE HERE
+def get_from_ipfs(cid, content_type="json"):
+    # get cid and create that and pass object parameter and need to make api call with credentials and cid
+    # change str to json object
+    # change json to dictionary and then return
+   # assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
+    # YOUR CODE HERE
 
-	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
-    params = ('arg', cid)
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=("2LVaqfpTeMvb7NuwrPSBBNvSL2u","17e2b174612e5eea2af96e5b7f55ebbf"))
-	return data
+    #assert isinstance(data, dict), f"get_from_ipfs should return a dict"
 
+
+params = ('arg', cid)
+response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params,
+                         auth=("2LVaqfpTeMvb7NuwrPSBBNvSL2u", "17e2b174612e5eea2af96e5b7f55ebbf"))
+return data
 
 files = {
     'file': '<pass>'
