20fda6d3a417b3e4bbee073cc8f7961114941907
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-03-13 18:52:36 -0400

    updated another time

--- mining_simulation.py
+++ mining_simulation.py
@@ -34,7 +34,8 @@ def Simulate(alpha, gamma, N, seed):
             # The selfish pool has 1 hidden block.
             if r <= alpha:
                 state = 2
-                SelfishRevenue += 1
+                SelfishRevenue += 2
+                ChainLength += 2
             # The selfish miners found a new block.
             # Write a piece of code to change the required variables.
             # You might need to define new variable to keep track of the number of hidden blocks.
