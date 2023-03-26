24d07b8a6ecb14dd930fe195e01eae5e9f484263
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-03-26 07:11:17 -0400

    Update AMM.sol

--- AMM.sol
+++ AMM.sol
@@ -70,10 +70,47 @@ contract AMM is AccessControl{
       swapAmt = ERC20(tokenA).balanceOf(address(this)) - qtyA;
 
       ERC20(tokenB).transferFrom( msg.sender, address(this), sellAmount);
+      ERC20(tokenA).transfer(msg.sender, swapAmt);
+    }
+
+    
+    uint256 new_invariant = ERC20(tokenA).balanceOf(address(this))*ERC20(tokenB).balanceOf(address(this));
+    require( new_invariant >= invariant, 'Bad trade' );
+    invariant = new_invariant;
+  }
+
+  /*
+    Use the ERC20 transferFrom to "pull" amtA of tokenA and amtB of tokenB from the sender
+  */
+  function provideLiquidity( uint256 amtA, uint256 amtB ) public {
+    require( amtA > 0 || amtB > 0, 'Cannot provide 0 liquidity' );
+    //YOUR CODE HERE
+    if (amtA > 0 ) {
+      ERC20(tokenA).transferFrom(msg.sender, address(this), amtA);
+    }
+    if (amtB > 0 ) {
+      ERC20(tokenB).transferFrom(msg.sender, address(this), amtB);
+    }
     invariant = ERC20(tokenA).balanceOf(address(this))*ERC20(tokenB).balanceOf(address(this));
-    emit Withdrawal( msg.sender, address(this), qtyA, qtyB );
+    emit LiquidityProvision( msg.sender, amtA, amtB );
+  }
+
+  /*
+    Use the ERC20 transfer function to send amtA of tokenA and amtB of tokenB to the target recipient
+    The modifier onlyRole(LP_ROLE) 
+  */
+  function withdrawLiquidity( address recipient, uint256 amtA, uint256 amtB ) public onlyRole(LP_ROLE) {
+    require( amtA > 0 || amtB > 0, 'Cannot withdraw 0' );
+    require( recipient != address(0), 'Cannot withdraw to 0 address' );
+    if( amtA > 0 ) {
+      ERC20(tokenA).transfer(recipient,amtA);
+    }
+    if( amtB > 0 ) {
+      ERC20(tokenB).transfer(recipient,amtB);
+    }
+    invariant = ERC20(tokenA).balanceOf(address(this))*ERC20(tokenB).balanceOf(address(this));
+    emit Withdrawal( msg.sender, recipient, amtA, amtB );
   }
 
 
-}
 }
