dd439299c6be6de0d6e8d52499c28330b1957a6b
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-04-04 10:01:29 -0400

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -8,42 +8,43 @@ import time
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.toChecksumAddress(bayc_address)
 
-#You will need the ABI to connect to the contract
-#The file 'abi.json' has the ABI for the bored ape contract
-#In general, you can get contract ABIs from etherscan
-#https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+# You will need the ABI to connect to the contract
+# The file 'abi.json' has the ABI for the bored ape contract
+# In general, you can get contract ABIs from etherscan
+# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
 with open('/home/codio/workspace/abi.json', 'r') as f:
-	abi = json.load(f) 
+    abi = json.load(f)
 
 ############################
-#Connect to an Ethereum node
-api_url = f"https://celo-mainnet.infura.io/v3/ebcd0dee40f2486396df85334c6879b4" #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+# Connect to an Ethereum node
+api_url = f"https://celo-mainnet.infura.io/v3/ebcd0dee40f2486396df85334c6879b4"  # YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
+
 def get_ape_info(apeID):
-	assert isinstance(apeID,int), f"{apeID} is not an int"
-	assert 1 <= apeID, f"{apeID} must be at least 1"
+    assert isinstance(apeID, int), f"{apeID} is not an int"
+    assert 1 <= apeID, f"{apeID} must be at least 1"
 
-	data = {'owner': "", 'image': "", 'eyes': "" }
-	
-	#YOUR CODE HERE	
-	contract = web3.eth.contract(address=contract_address,abi=abi)
-    	owner = contract.functions.ownerOf(apeID).call()
-    	data['owner'] = owner
-    	endpoint = 'https://gateway.pinata.cloud/ipfs/'
-    
-    	token_url = contract.functions.tokenURI(apeID).call()
-    	response = requests.get(endpoint + token_url)
-    	response_data = json.loads(response.text)
-    	data['image'] = response_data['image']
-    	for i in response_data['attributes']:
-        	if (i['trait_type'] == 'Eyes'):
-            		data['eyes'] = i['value']
-            			break
+    data = {'owner': "", 'image': "", 'eyes': ""}
 
-	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
-	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
-	return data
+    # YOUR CODE HERE
+    contract = web3.eth.contract(address=contract_address, abi=abi)
+    owner = contract.functions.ownerOf(apeID).call()
+    data['owner'] = owner
+    endpoint = 'https://gateway.pinata.cloud/ipfs/'
+
+    token_url = contract.functions.tokenURI(apeID).call()
+    response = requests.get(endpoint + token_url)
+    response_data = json.loads(response.text)
+    data['image'] = response_data['image']
+    for i in response_data['attributes']:
+        if (i['trait_type'] == 'Eyes'):
+            data['eyes'] = i['value']
+            break
 
 
+assert isinstance(data, dict), f'get_ape_info{apeID} should return a dict'
+assert all([a in data.keys() for a in
+            ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
+return data
