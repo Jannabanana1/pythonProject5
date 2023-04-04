ffd59f0f21be799e8025e49296ca2ccdb02c2e17
Jannabanana1 <66581721+Jannabanana1@users.noreply.github.com>
2023-04-04 10:02:41 -0400

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -44,7 +44,7 @@ def get_ape_info(apeID):
             break
 
 
-assert isinstance(data, dict), f'get_ape_info{apeID} should return a dict'
-assert all([a in data.keys() for a in
+    assert isinstance(data, dict), f'get_ape_info{apeID} should return a dict'
+    assert all([a in data.keys() for a in
             ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
-return data
+    return data
